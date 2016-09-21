584649bf12ae84a36bca939f977c509e14f9910d
Brian McDevitt <brianmcd@terpmail.umd.edu>
2016-09-21 14:55:45 -0400

    Initial Commit

---
+++ Homepwner/DetailViewController.swift
@@ -0,0 +1,114 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Brian McDevitt on 6/9/16.
+//  Copyright © 2016 Brian McDevitt. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    var item: Item! {
+        didSet {
+            navigationItem.title = item.name
+        }
+    }
+    var imageStore: ImageStore!
+    
+    let numberFormatter: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .DecimalStyle
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .MediumStyle
+        formatter.timeStyle = .NoStyle
+        return formatter
+    }()
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.stringFromNumber(item.valueInDollars)
+        dateLabel.text = dateFormatter.stringFromDate(item.dateCreated)
+        
+        //Get the item key
+        let key = item.itemKey
+        
+        //If there is an associated image with the item display it on the image view
+        let imageToDisplay = imageStore.imageForKey(key)
+        imageView.image = imageToDisplay
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        //Clear first responder
+        view.endEditing(true)
+        
+        //"Save changes to item
+        item.name = nameField.text ?? ""
+        item.serialNumber = serialNumberField.text
+        
+        if let valueText = valueField.text, value = numberFormatter.numberFromString(valueText) {
+            item.valueInDollars = value.integerValue
+        } else {
+            item.valueInDollars = 0
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func backgroundTapped(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func takePicture(sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        
+        //If the devide has a camera, take a picture; otherwise, 
+        //just pick from photo library
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            imagePicker.sourceType = .Camera
+        } else {
+            imagePicker.sourceType = .PhotoLibrary
+        }
+        
+        imagePicker.delegate = self
+        
+        //Place image picker on the screen
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //Get picked image from inro dictionary
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        //Store the image in the ImageStore for the item's key
+        imageStore.setImage(image, forKey: item.itemKey)
+        
+        //Put that image on the screen in the image view
+        imageView.image = image
+        
+        //Take image picker off the screen - you must call this dismiss method
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Brian McDevitt on 6/9/16.
+//  Copyright © 2016 Brian McDevitt. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    func updateLabels() {
+        let bodyFont = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        nameLabel.font = bodyFont
+        valueLabel.font = bodyFont
+        
+        let caption1Font = UIFont.preferredFontForTextStyle(UIFontTextStyleCaption1)
+        serialNumberLabel.font = caption1Font
+    }
+}
